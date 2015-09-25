3339417b981b8845e19f8a6edc859dd6bf4f6823
Tobias Hintze <thintze+git@tpip.net>
2015-09-25 16:25:46 +0200

    Update to conform 0.17.0-x1-2-g42b76b9
    ( fixes https://github.com/bitwalker/conform/issues/45 )

--- mix.exs
+++ mix.exs
@@ -15,7 +15,7 @@ defmodule ReleaseManager.Mixfile do
   def application, do: []
 
   def deps do
-    [{:conform, "~> 0.17.0-x1", github: "xerions/conform"},
+    [{:conform, github: "xerions/conform", branch: "master"},
      {:relx, "~> 3.5.0" },
      {:earmark, "~> 0.1", only: :dev},
      {:ex_doc, "~> 0.10", only: :dev},
